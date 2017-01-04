c62ad9bcd12742c1ed82d89b3b50acd65264000d
grzeg <grzeg@LAPTOP-GRZES>
2017-01-04 21:57:30 +0100

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='PyRPN',
+      version='1.0',
+      description='Python Reverse Polish Notation calculator',
+      author='Grzegorz Kotarski',
+      author_email='grzes.kotarski@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['pyrpn', 'pyrpn.tests'],
+     )
